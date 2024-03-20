e0e4352d558e0f9fca5aa9f88a37ef42a5783c4a
DongWook Lee <sh95119@gmail.com>
2024-03-20 22:44:15 +0900

    FEAT: 기본적인 레이아웃 예제 생성
    테일윈드 CSS 컴포넌트를 참조하였음
    
    ref: https://tailwindui.com/components

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router';
+import Header from './Header';
+import Footer from './Footer';
 
 export function Layout() {
   return (
     <div>
-      레이아웃(사이드바 등등)
-      <Outlet />
+      <Header />
+      <main className="h-screen">
+        <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <Outlet />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
